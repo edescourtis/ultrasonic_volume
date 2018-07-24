4a1b0777a8f39d1e0006ff4668b2a692ca338344
Eric des Courtis <eric.descourtis@mitel.com>
2018-07-24 10:47:31 -0400

    Initial commit of ultrasonic volume control project for Simon
      * Install dependencies with 'npm install'
      * Use 'npm run start' to run the program

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/unambiguous */
+import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
+
+range(1, 20)
+  .pipe(filter(x => x % 2 === 1), map(x => x + x))
+  .subscribe(x => console.log(x));
+
+const name = `Simon`;
+
+console.log(`Hello ${name}!`);
